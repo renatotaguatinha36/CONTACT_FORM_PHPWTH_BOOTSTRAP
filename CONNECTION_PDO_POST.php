31e28bad13324a2ed4bc38288b378af73e6a6892
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-22 06:38:03 -0300

    ok

--- CONNECTION_PDO_POST.php
+++ CONNECTION_PDO_POST.php
@@ -87,5 +87,14 @@ try {
   echo $sql . "<br>" . $e->getMessage();
 }
 
+
+while($lista = $stmt->fetchAll(PDO::FETCH_ASSOC)){
+
+  echo  '<pre>';
+       print_r($lista);
+  echo  '</pre>';
+}
+
+
 $conn = null;
 ?>
