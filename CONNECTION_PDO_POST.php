fc28cba19a05e6432c397bef985ea5f571d9bfa8
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-22 06:24:24 -0300

    ok

--- CONNECTION_PDO_POST.php
+++ CONNECTION_PDO_POST.php
@@ -71,6 +71,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
   $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
 
