7681571b1a6d7c0e7670689779a9490e953e2067
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-26 06:29:12 -0300

    ok

--- CONNECTION_PDO_POST.php
+++ CONNECTION_PDO_POST.php
@@ -140,6 +140,7 @@ try {
 
   $last_id = $conn->lastInsertId();
   echo "New record created successfully. Last inserted ID is: " . $last_id;
+  echo $stmt->rowCount() ." Records UPDATE Sucessfully ";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
