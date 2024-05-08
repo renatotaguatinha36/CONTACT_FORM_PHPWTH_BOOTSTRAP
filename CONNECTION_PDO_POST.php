7858f718ab27338bf52113defb4374b3fa06d81d
renatoguara2020 <renatoguara2020@gmail.com>
2024-05-08 08:40:46 -0300

    ok

--- CONNECTION_PDO_POST.php
+++ CONNECTION_PDO_POST.php
@@ -36,7 +36,7 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_CASE, PDO::CASE_UPPER);
-  $stmt = $conn->prepare("INSERT INTO MyGuests (id, firstname, lastname, email, senha) VALUES (:id, :firstname, :lastname, :email, :senha)");
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email, senha) VALUES (:firstname, :lastname, :email, :senha)");
   $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
   $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
   $stmt->bindValue(':email', $email, PDO::PARAM_STR);
