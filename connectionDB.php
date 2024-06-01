2fb8f56fa6ac5fab5d1915beb5d8bd45acac728e
renatoguara2020 <renatoguara2020@gmail.com>
2024-06-01 09:32:55 -0300

    ok

--- connectionDB.php
+++ connectionDB.php
@@ -9,5 +9,9 @@ $dbname = "myDBPDO";
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+
 
 ?>
