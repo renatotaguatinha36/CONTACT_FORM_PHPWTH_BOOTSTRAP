3bd676d0f5bd409e4db144d48d0f5520750e4cf6
renatoguara2020 <renatoguara2020@gmail.com>
2024-06-04 08:40:49 -0300

    ok

--- CONNECTION_PDO_POST.php
+++ CONNECTION_PDO_POST.php
@@ -75,7 +75,7 @@ while($lista = $stmt->fetchAll(PDO::FETCH_ASSOC)){
   echo  "</pre>";
 }
 
-$querySQL = "SELECT * FROM tb_usuarios WHERE id= $id "; 
+$querySQL = "SELECT * FROM tb_usuarios WHERE id = $id "; 
 
 $stmt = $conn->query($querySQL); //PDO Statemet
 $stmt = $conn->prepare($querySQL); // Com método prepare()
